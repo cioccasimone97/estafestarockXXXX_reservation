19126d5aa37c1684f1f6dbbe120c4058fe6f2c63
Simone <simoneciocca@rocketmail.com>
2024-06-29 11:21:27 +0200

    Update prenota.php

--- prenota.php
+++ prenota.php
@@ -16,10 +16,18 @@ VALUES ('$nominativo', '$telefono', '$data_evento', '$ora_evento', $numero_perso
 if ($conn->query($sql) === TRUE) {
     // Numero di telefono predefinito per il messaggio WhatsApp
     $whatsapp_number = '3492982845';
-    $message = urlencode("Nuova prenotazione:\nNominativo: $nominativo\nTelefono: $telefono\nData: $data_evento\nOra: $ora_evento\nNumero di persone: $numero_persone\nNote: $note");
-    $whatsapp_url = "https://wa.me/$whatsapp_number?text=$message";
-    echo "<script type='text/javascript'>window.location.href='$whatsapp_url';</script>";
-    echo "Prenotazione effettuata con successo! Verrai reindirizzato a WhatsApp.";
+    $message = "Nuova prenotazione:\nNominativo: $nominativo\nTelefono: $telefono\nData: $data_evento\nOra: $ora_evento\nNumero di persone: $numero_persone\nNote: $note";
+    $encoded_message = urlencode($message);
+    $whatsapp_url = "https://wa.me/$whatsapp_number?text=$encoded_message";
+    
+    // Messaggio
+    $confirm_message = "Prenotazione effettuata con successo! Ora verrai reindirizzato a WhatsApp, invia il messaggio così come proposto senza apportare modifiche";
+    
+    // Stampa l'alert e il reindirizzamento
+    echo "<script type='text/javascript'>
+            alert('$confirm_message');s
+            window.location.href='$whatsapp_url';
+          </script>";
 } else {
     echo "Errore: " . $sql . "<br>" . $conn->error;
 }
